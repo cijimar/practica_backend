3b58cec440e6577d09a9391cbeee75c4866f5105
Cintia Jiménez <cijimar@gmail.com>
2026-05-04 18:40:17 +0200

    feat: implementar UsuarioService

---
+++ FLYWAY/src/main/java/es/ediae/master/programacion/gestionusuario/service/impl/UsuarioService.java
@@ -0,0 +1,52 @@
+package es.ediae.master.programacion.gestionusuario.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import es.ediae.master.programacion.gestionusuario.entity.UsuarioEntity;
+import es.ediae.master.programacion.gestionusuario.repository.UsuarioRepository;
+import es.ediae.master.programacion.gestionusuario.service.IUsuarioService;
+
+@Service
+public class UsuarioService implements IUsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public List<UsuarioEntity> obtenerTodosLosUsuarios() {
+        return usuarioRepository.findAll();
+    }
+
+    @Override
+    public UsuarioEntity obtenerUsuarioPorId(Integer id) {
+        return usuarioRepository.findById(id).orElse(null);
+    }   
+
+    @Override
+    public UsuarioEntity crearUsuario(UsuarioEntity usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public UsuarioEntity actualizarUsuario(Integer id, UsuarioEntity usuario) {
+        usuario.setId(id);
+        return usuarioRepository.save(usuario);
+    }
+
+    @Override
+    public void eliminarUsuario(Integer id) {
+        usuarioRepository.deleteById(id);
+    }
+
+    @Override
+    public UsuarioEntity obtenerUsuarioPorEmail(String email) {
+        return usuarioRepository.findByEmail(email);
+    }
+
+    
+    
+    
+}
