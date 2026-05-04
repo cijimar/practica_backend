7078362cb30db1e789940d0a932fd80d6e323e42
Cintia Jiménez <cijimar@gmail.com>
2026-05-04 17:26:54 +0200

    feat: implementar DireccionRepository

---
+++ FLYWAY/src/main/java/es/ediae/master/programacion/gestionusuario/repository/DireccionRepository.java
@@ -0,0 +1,20 @@
+package es.ediae.master.programacion.gestionusuario.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import es.ediae.master.programacion.gestionusuario.entity.DireccionEntity;
+
+@Repository
+public interface DireccionRepository extends JpaRepository<DireccionEntity, Integer> {
+    
+    @Query("SELECT d FROM DireccionEntity d WHERE d.usuario.id = :usuarioId")
+    //Cambiamos el findByUsuarioId por buscarPorUsuarioId porque Spring hace la implementación de forma automática y nos puede dar error si el nombre no sigue la convención de Spring Data JPA 
+    List<DireccionEntity> buscarPorUsuarioId(Integer usuarioId);
+
+
+
+}
