63b6993a0ca3d094b08a7698d132329b134ac381
Cintia Jiménez <cijimar@gmail.com>
2026-05-05 15:57:49 +0200

    fix: obtenerUsuarioPorEmail deleted

--- FLYWAY/src/main/java/es/ediae/master/programacion/gestionusuario/service/IUsuarioService.java
+++ FLYWAY/src/main/java/es/ediae/master/programacion/gestionusuario/service/IUsuarioService.java
@@ -8,7 +8,6 @@ public interface IUsuarioService {
 
     List<UsuarioEntity> obtenerTodosLosUsuarios();
     UsuarioEntity obtenerUsuarioPorId(Integer id);
-    UsuarioEntity obtenerUsuarioPorEmail(String email);
     UsuarioEntity crearUsuario(UsuarioEntity usuario);
     UsuarioEntity actualizarUsuario(Integer id, UsuarioEntity usuario);
     void eliminarUsuario(Integer id);
