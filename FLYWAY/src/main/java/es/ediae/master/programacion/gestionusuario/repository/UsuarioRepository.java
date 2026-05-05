b92d6e0963800f843df1872dc7b353495f67ed24
Cintia Jiménez <cijimar@gmail.com>
2026-05-05 19:49:22 +0200

    fix: Query

---
+++ FLYWAY/src/main/java/es/ediae/master/programacion/gestionusuario/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package es.ediae.master.programacion.gestionusuario.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import es.ediae.master.programacion.gestionusuario.entity.UsuarioEntity;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Integer> {
+
+    @Query("SELECT u FROM UsuarioEntity u WHERE u.nickUsuario = :nickUsuario")
+    UsuarioEntity findByNickUsuario(@Param("nickUsuario") String nickUsuario);
+
+}
